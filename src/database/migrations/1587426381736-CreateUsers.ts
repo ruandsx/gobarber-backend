3acd1984ac3c9fc2adeb091e5e17d40489805317
Ruan Xavier <ruandsxavier@gmail.com>
2020-04-20 22:19:51 -0300

    Creating users and hashing passwords

--- src/database/migrations/1587426381736-CreateUsers.ts
+++ src/database/migrations/1587426381736-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1587426381736 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
