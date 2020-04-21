d4fe00a21cabf734c4278cc2ecdc8dfd105707d3
Ruan Xavier <ruandsxavier@gmail.com>
2020-04-21 14:07:19 -0300

    serving static files

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -19,5 +25,27 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ message: error.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    try {
+      const { user, file } = request;
+
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const userUpdated = await updateUserAvatar.execute({
+        user_id: user.id,
+        avatarFilename: file.filename,
+      });
+
+      return response.json(userUpdated);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
+  }
+);
 
 export default usersRouter;
