fb64fdcfbf7767b30b609863f60d45a35a3bc2ec
Ruan Xavier <ruandsxavier@gmail.com>
2020-07-02 15:22:27 -0300

    creating fake repository and writing unit test

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,7 +1,20 @@
-function sum(a: number, b: number): number {
-  return a - b;
-}
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 
-test('sum two numbers', () => {
-  expect(sum(1, 2)).toBe(3);
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '1234',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('1234');
+  });
 });
