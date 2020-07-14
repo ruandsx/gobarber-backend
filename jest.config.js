58e90d9a0847c51772365523e2ae83d5123b85ae
Ruan Xavier <ruandsxavier@gmail.com>
2020-07-13 23:08:55 -0300

    improving jest coverage

--- jest.config.js
+++ jest.config.js
@@ -21,10 +21,8 @@ module.exports = {
   collectCoverageFrom: [
     '<rootDir>/src/modules/**/services/*.ts'
   ],
-
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
-
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
   //   "\\\\node_modules\\\\"
