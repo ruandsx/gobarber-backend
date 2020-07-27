70569084b234fb98af10ddcf88d7bafd59766510
Ruan Xavier <ruandsxavier@gmail.com>
2020-07-27 09:58:27 -0300

    creating provider availability dates controllers

---
+++ src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
@@ -0,0 +1,22 @@
+import { container } from 'tsyringe';
+import { Request, Response } from 'express';
+
+import ListProviderMonthAvailabilityService from '@modules/appointments/services/ListProviderMonthAvailabilityService';
+
+export default class ProviderMonthAvailabilityController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const { provider_id, month, year } = request.body;
+
+    const listProviderMonthAvailability = container.resolve(
+      ListProviderMonthAvailabilityService
+    );
+
+    const providers = await listProviderMonthAvailability.execute({
+      provider_id,
+      month,
+      year,
+    });
+
+    return response.json(providers);
+  }
+}
